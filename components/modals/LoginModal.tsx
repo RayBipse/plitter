46d1ad0a9bdbd1614b91551ec74fda4f47d86b8a
RayBipse <raybipse@gmail.com>
2024-04-07 13:52:36 -0700

    Changed to Plitter

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -53,7 +53,7 @@ const LoginModal: React.FC = () => {
     const footerContent = (
         <div className="text-neutral-400 text-center mt-4">
             <p>
-                First time using Twitter?
+                First time using Plitter?
                 <span onClick={onToggle} className="text-white cursor-pointer hover:underline">
                     {" "}
                     Create an account
